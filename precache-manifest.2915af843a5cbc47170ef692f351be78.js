8cece00d953deaa477b4394d3881ed85708e207c
manjeet-123 <manjeetsingh08484@gmail.com>
2020-10-02 15:42:33 +0530

    Updates

---
+++ precache-manifest.2915af843a5cbc47170ef692f351be78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acb10c06c32697bafb6740208254ab0",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "c22d7a57fd542f5a70eb",
+    "url": "/monsters-relodex/static/css/main.c6befb00.chunk.css"
+  },
+  {
+    "revision": "01c0ca26252d8e64ddad",
+    "url": "/monsters-relodex/static/js/2.b1f13336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.b1f13336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22d7a57fd542f5a70eb",
+    "url": "/monsters-relodex/static/js/main.58430df0.chunk.js"
+  },
+  {
+    "revision": "3dbb1ac3b3ec63e88814",
+    "url": "/monsters-relodex/static/js/runtime-main.31087287.js"
+  }
+]);
